6e39b119005188383cc313c311faf7504af7a7d4
Patrick Naughton <patricknaughton01@gmail.com>
2018-11-22 20:44:56 -0600

    Request Challenge Button
    The request challenge button is now only shown to appropriate users
    (users who have a better rank than you, who are within four ranks of you,
    and against whom you have not already requested a match).

--- badminton/src/components/Ranking.js
+++ badminton/src/components/Ranking.js
@@ -26,6 +26,7 @@ class Ranks extends Component {
       snapshot.forEach(function(childSnap){
         var user_obj = childSnap.val();
         user_obj.key = childSnap.key;
+        user_obj.challengeButton = false;
         newUsers.push(user_obj);
       });
       this.setState(byPropKey('users', newUsers));
@@ -48,19 +49,52 @@ class RankList extends Component{
       users: this.props.users,
       alert: null,
     };
+    this.requestChallenge = this.requestChallenge.bind(this);
   }
 
   requestChallenge(key, username){
-    database.ref(`pending-challenge-requests`).push({
-      'challenger': firebase.auth.currentUser.uid,
-      'opponent': key,
+    if(firebase.auth.currentUser.uid !== key){
+      database.ref(`pending-challenge-requests`).push({
+        'challenger': firebase.auth.currentUser.uid,
+        'opponent': key,
+      });
+      this.setState(byPropKey('alert', <Alert
+        text={'Your request was received and is being processed.'}
+        color={'#00ff00'}
+        />
+      ));
+      setTimeout(() => {this.setState(byPropKey('alert', null))}, 4000);
+    }else{
+      this.setState(byPropKey('alert', <Alert
+        text={'You cannot challenge yourself.'}
+        color={'#ff0000'}
+      />));
+      setTimeout(() => {this.setState(byPropKey('alert', null))}, 3000);
+    }
+  }
+
+  componentDidMount(){
+    database.ref(`users/${firebase.auth.currentUser.uid}`).once('value', (snapshot) => {
+      var snapUser = snapshot.val();
+      var rank = snapUser.rank;
+      var id = snapshot.key;
+      var users = this.state.users;
+      database.ref(`pending-challenge-requests`).on('value', (requestSnap) => {
+        var requests = requestSnap.val();
+        for(var i = 0; i<users.length; i++){
+          var rankDiff = rank - users[i].rank;
+          if(rankDiff > 0 && rankDiff <= 4){
+            users[i].challengeButton = true;
+          }
+          for(var key in requests){
+            if(requests[key].challenger === id && requests[key].opponent === users[i].key){
+              users[i].challengeButton = false;
+            }
+          }
+        }
+        this.setState(byPropKey('users', users));
+      });
     });
-    this.setState(byPropKey('alert', <Alert
-      text={'Your request was received and is being processed.'}
-      color={'#00ff00'}
-      />
-    ));
-    setTimeout(() => {this.setState(byPropKey('alert', null))}, 5000);
   }
 
   render(){
@@ -73,9 +107,7 @@ class RankList extends Component{
           <div className="rank-username">
             {user.username}
           </div>
-          <div>
-            <button onClick={() => this.requestChallenge(user.key, user.username)}>Request Challenge</button>
-          </div>
+          {user.challengeButton && <ChallengeButton user={user} onClick={this.requestChallenge}/>}
         </div>
       </div>
     );
@@ -87,6 +119,11 @@ class RankList extends Component{
     );
   }
 }
+
+const ChallengeButton = ({ user, onClick }) =>
+  <div>
+    <button onClick={() => onClick(user.key, user.username)}>Request Challenge</button>
+  </div>
 
 const authCondition = (authUser) => !!authUser;
 
