7a3c96e7b22fc1552515b0194658d0f9f01cded4
Stefan Quach <stefantquach@bitbucket.org>
2018-11-28 11:31:21 -0600

    Created edit button

--- badminton/src/components/CreateAnnouncement.js
+++ badminton/src/components/CreateAnnouncement.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { firebase, db } from '../firebase'
+import { firebase, db, auth } from '../firebase'
 import { db as database } from '../firebase/firebase';
 import { byPropKey, adminCheck, convertUnixTime } from '../helpers/helpers';
 import { withRouter } from 'react-router-dom';
@@ -18,7 +18,7 @@ class CreateAnnouncementPage extends Component{
 
     this.state = {...INITIAL_STATE};
   }
-
+//componentDidMount
   onSubmit(event) {
     event.preventDefault();
 
@@ -71,7 +71,7 @@ class CreateAnnouncementPage extends Component{
               onChange={event => this.setState(byPropKey('content', event.target.value))}
               value={content}></textarea>
           </div>
-          <div className="form-grou">
+          <div className="form-group">
             <input
               id="announcement-public"
               value={false}
@@ -91,19 +91,39 @@ class CreateAnnouncementPage extends Component{
 
 const authCondition = adminCheck;
 
-const Announcements = ({ announcements }) =>{
+const Announcements = ({ announcements }) => {
   var announcementElems = announcements.map((ann) =>
     <div className="announcement" key={ann.key}>
       <h3>{ann.title}</h3>
       <div className="announcement-caption">By: {ann.author}</div>
-      <div className="announcement-caption">Written at: {convertUnixTime(ann.timestamp)}</div>
+      <div className="announcement-caption">Last Updated {convertUnixTime(ann.timestamp)}</div>
       <div className="announcement-content">{ann.content}</div>
+      <AdminButton text={"Edit"} onClick={() => console.log('edit')}/>
       <hr/>
     </div>
   );
   return(
     <div>{announcementElems}</div>
   );
+}
+
+class AdminButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    adminCheck(firebase.auth.currentUser).then((isAdmin) => {
+      this.setState({admin: isAdmin});
+    });
+
+  }
+
+  render() {
+    return(
+      <div>{this.state.admin ? <button onClick={this.props.onClick}>{this.props.text}</button> : <div></div>}</div>
+    );
+  }
 }
 
 export default withAuthorization(authCondition)(withRouter(CreateAnnouncementPage));
